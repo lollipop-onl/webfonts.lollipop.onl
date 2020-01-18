1c70155dddc040759caf47b13c8e4c147fa9a877
lollipop-onl <lollipop@simoochee.net>
2020-01-19 03:43:14 +0900

    🐛 templateのシンタックスを修正

--- scripts/const/css-template.js
+++ scripts/const/css-template.js
@@ -3,25 +3,6 @@ const C = require('../const');
 
 module.exports = _.template(`
 <% _.forEach(fonts, function(font) { %>
-<% const { fontFile = '', fontStyle = 'normal', fontWeight = 400, fontDisplay = 'swap', locals = [] } = font %>
-<% const fontFileName = fontFile.replace(/^(.*)\\..*$/, '$1') %>
-
-<% _.forEach(C.UNICODE_RANGES, function(ranges, index) { %>
-
-@font-face {
-  font-family: <%= fontName %>;
-  font-style: <%= fontStyle %>;
-  font-weight: <%= fontWeight %>;
-  font-display: <%= fontDisplay %>;
-  src:
-    <% _.forEach(locals, function(local) { %>
-    local('<%= local %>'),
-    <% }) %>
-    url('/<%= fontFileName %>.<%= index %>.woff2') format('woff2');
-  unicode-range: <%= ranges.join(', ') %>;
-}
-
-<% }) %>
 
 <% }) %>
 `, {
