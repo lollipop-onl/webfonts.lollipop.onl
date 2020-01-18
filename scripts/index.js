0ac52a5de842c0b9d7db10310702ba084f7901de
lollipop-onl <lollipop@simoochee.net>
2020-01-18 18:48:26 +0900

    ✨ マルチプロセス対応

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,34 @@
+// const fontRanger = require('font-ranger/lib/font-ranger');
+// const UNICODE_RANGES = require('./unicode-ranges');
+//
+// const DEFAULT_PARAMETER = {
+//   fontFile: 'docs/GenEiAntiqueNv5-M.ttf',
+//   outputFolder: './docs',
+//   urlPrefix: '/fonts/',
+//   fontFamily: 'GenEiAntique',
+//   fontWeight: 400,
+//   fontStyle: 'normal',
+//   fontDisplay: 'swap',
+//   keepFormat: false,
+//   addWoff: true,
+//   skipCss: false,
+//   copyOriginal: false,
+//   locals: ['GenEiAntiqueNv5-M'],
+// };
+//
+// console.log('happen');
+//
+// UNICODE_RANGES.forEach((ranges, index) => {
+//   const parameter = {
+//     ...DEFAULT_PARAMETER,
+//     ranges,
+//     fontName: `GenEiAntique.${index}`
+//   };
+//
+//   fontRanger(parameter);
+// });
+
+const { Worker, isMainThread, workerData } = require('worker_threads');
 const fontRanger = require('font-ranger/lib/font-ranger');
 const UNICODE_RANGES = require('./unicode-ranges');
 
@@ -16,14 +47,41 @@ const DEFAULT_PARAMETER = {
   locals: ['GenEiAntiqueNv5-M'],
 };
 
-console.log('happen');
+let failedProcessIndexes = [];
 
-UNICODE_RANGES.forEach((ranges, index) => {
-  const parameter = {
-    ...DEFAULT_PARAMETER,
-    ranges,
-    fontName: `GenEiAntique.${index}`
-  };
+if (isMainThread) {
+  console.log('BEGIN: MainThread process');
 
-  fontRanger(parameter);
-});
+  UNICODE_RANGES.forEach((ranges, index) => {
+    new Worker(__filename, {
+      workerData: {
+        ranges,
+        index
+      }
+    });
+  });
+
+  console.log('FINISH: MainThread process');
+
+  if (failedProcessIndexes.length > 0) {
+    console.error('Failed process indexes is:', failedProcessIndexes.join(','));
+  }
+} else {
+  const { ranges, index } = workerData;
+
+  console.log(`BEGIN: ChildThread#${index} process`);
+
+  try {
+    fontRanger({
+      ...DEFAULT_PARAMETER,
+      ranges,
+      fontName: `GenEiAntique.${index}`
+    });
+  } catch (err) {
+    console.error(err);
+
+    failedProcessIndexes.push(index);
+  }
+
+  console.log(`FINISH: ChildThread#${index} process`);
+}
