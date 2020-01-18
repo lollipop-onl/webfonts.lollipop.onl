9483f4051befcf53e6581aad3ff5d344dd17a1b1
lollipop-onl <lollipop@simoochee.net>
2020-01-18 18:02:40 +0900

    ✨ サブセット化するスクリプトを追加

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+const fontRanger = require('font-ranger/lib/font-ranger');
+const UNICODE_RANGES = require('./unicode-ranges');
+
+const DEFAULT_PARAMETER = {
+  fontFile: 'docs/GenEiAntiqueNv5-M.ttf',
+  outputFolder: './docs',
+  urlPrefix: '/fonts/',
+  fontFamily: 'GenEiAntique',
+  fontWeight: 400,
+  fontStyle: 'normal',
+  fontDisplay: 'swap',
+  keepFormat: false,
+  addWoff: true,
+  skipCss: false,
+  copyOriginal: false,
+  locals: ['GenEiAntiqueNv5-M'],
+};
+
+console.log('happen');
+
+UNICODE_RANGES.forEach((range, index) => {
+  const parameter = {
+    ...DEFAULT_PARAMETER,
+    ranges: UNICODE_RANGES,
+    fontName: `GenEiAntique.${index}`
+  };
+
+  console.log(parameter);
+
+  try {
+    fontRanger(parameter);
+  } catch (err) {
+    console.error(err);
+  }
+});
