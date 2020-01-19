ab68a0ea91b6af052d2130b67b39a72c4895ff4c
lollipop-onl <lollipop@simoochee.net>
2020-01-19 19:22:24 +0900

    🔧 Metaとrobots.txtを追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ import { Configuration } from '@nuxt/types';
 const config: Configuration = {
   mode: 'spa',
   srcDir: 'app',
+  head: {
+    title: 'lollipop.onl Webfonts',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+  },
   buildModules: ['@nuxt/typescript-build'],
   modules: ['@nuxt/http'],
   css: ['reset-css'],
